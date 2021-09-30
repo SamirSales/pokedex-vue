46802e0929de152db03ffa91812d9857598bae01
Samir Sales <samir.sribeiro@gmail.com>
2021-09-29 21:53:52 -0300

    #master add prettier file + format src files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+    <hello-world />
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import HelloWorld from '../components/HelloWorld';
 
 export default {
-  name: "Home",
+    name: 'Home',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld
+    }
 };
 </script>
