b01867510ba8cbb59623b53a42f878fe87348f48
Samir Sales <samir.sribeiro@gmail.com>
2021-09-29 22:05:43 -0300

    #master add i18n

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <h2>{{ $t('message') }}</h2>
     </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
+    transpileDependencies: ['vuetify'],
+
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
+    }
 };
